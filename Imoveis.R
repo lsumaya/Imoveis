ac6a9207e164608e1b5054652899036f123e1745
lsumaya <lsumaya@id.uff.br>
2026-04-01 22:03:49 -0300

    Commit 3: Ajuste no codigo

--- Imoveis.R
+++ Imoveis.R
@@ -1,17 +1,22 @@
 # Script referente a atividade de extensão 3
+#Carregando pacotes
 if(!require(ggplot2)) install.packages("ggplot2")
 library(ggplot2)
+if(!require(dplyr)) install.packages("dplyr") # Necessário para a função glimpse
+library(dplyr)
 install.packages("usethis")
 library("usethis")
 
 #Leitura dos dados
 dados = read.csv("imoveis.csv",header = T,sep = ";")
-attach(dados)
 names(dados) <- trimws(names(dados))
 
 #Análise descritiva - variáveis quantitativas
 summary(dados)
 str(dados)
+glimpse(dados)
+View(head(dados))
+
 # Histograma do preço
 ggplot(dados, aes(x = preco)) +
   geom_histogram(bins = 10, fill = "purple", color = "white") +
@@ -19,5 +24,9 @@ ggplot(dados, aes(x = preco)) +
 
 # Dispersão: Imposto (y) vs Metragem (x)
 ggplot(dados, aes(x = metragem, y = imposto_anual)) +
-  geom_point() +
-  labs(title = "Dispersão: Imposto vs Metragem", x = "Metragem", y = "Imposto")
+  geom_point(color = "purple", size = 2) +
+  labs(title = "Dispersão: Imposto Anual vs Metragem", 
+       x = "Metragem (m²)", 
+       y = "Imposto Anual (R$)") +
+  theme_minimal()
+
