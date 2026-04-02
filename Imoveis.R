3f6f2ddfad049138318895a5befeecca44888cbe
lsumaya <lsumaya@id.uff.br>
2026-04-01 21:00:36 -0300

    Commit 2:Adicao de graficos

--- Imoveis.R
+++ Imoveis.R
@@ -12,3 +12,12 @@ names(dados) <- trimws(names(dados))
 #Análise descritiva - variáveis quantitativas
 summary(dados)
 str(dados)
+# Histograma do preço
+ggplot(dados, aes(x = preco)) +
+  geom_histogram(bins = 10, fill = "purple", color = "white") +
+  labs(title = "Histograma do Preço", x = "Preço", y = "Frequência")
+
+# Dispersão: Imposto (y) vs Metragem (x)
+ggplot(dados, aes(x = metragem, y = imposto_anual)) +
+  geom_point() +
+  labs(title = "Dispersão: Imposto vs Metragem", x = "Metragem", y = "Imposto")
