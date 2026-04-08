b0d23e1c45fc0ac8fd6da70644e0f31df20febf6
lsumaya <lsumaya@id.uff.br>
2026-04-07 21:19:03 -0300

    Script esqueleto

---
+++ imoveis-com-branches.R
@@ -0,0 +1,19 @@
+# Script para análise de imóveis com uso de branches
+
+# 1. Leitura do banco de dados
+
+dados = read.csv("imoveis.csv", header = TRUE, sep = ";")
+names(dados) = trimws(names(dados))
+
+# ver estrutura
+str(dados)
+head(dados)
+
+
+# 2. Gráficos
+# (PREENCHER NA BRANCH graficos)
+
+
+
+# 3. Medidas descritivas
+# (PREENCHER NA BRANCH medidas)
